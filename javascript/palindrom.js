07515f306763c01e626f0c001a5e637854501afb
Dmytro <dziurziadmytro@gmail.com>
2025-02-24 21:11:16 +0100

    add palindrom for str

--- javascript/palindrom.js
+++ javascript/palindrom.js
@@ -10,3 +10,15 @@ function palindrom(word) {
 }
 
 console.log(palindrom("aa aabbaa aa"));
+
+/**
+ * Checks if the input string is a palindrome, ignoring punctuation, spaces, and case.
+ * @param {string} str - The string to check.
+ * @returns {boolean} True if palindrome, otherwise false.
+ */
+function isPalindrome(str) {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return cleaned === cleaned.split("").reverse().join("");
+}
+
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
